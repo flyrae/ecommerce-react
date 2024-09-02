f3cd0e2b5297803dddf10fbc0d6bd6e6d6d34e22
flyrae <flyrae@163.com>
2024-09-02 14:35:18 +0800

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -32,12 +32,12 @@ firebase.auth.onAuthStateChanged((user) => {
   render(<App store={store} persistor={persistor} />, root);
 });
 
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('SW registered: ', registration);
-    }).catch((registrationError) => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
+// if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js').then((registration) => {
+//       console.log('SW registered: ', registration);
+//     }).catch((registrationError) => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
--- src/views/checkout/step2/ShippingForm.jsx
+++ src/views/checkout/step2/ShippingForm.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import { CustomInput, CustomMobileInput } from '@/components/formik';
 import { Field, useFormikContext } from 'formik';
 import React from 'react';
